9195314da1799ee547d5f81c537468766b891504
Venkatraman <ramsunvtech@gmail.com>
2020-06-17 00:07:41 +0800

    docs(site): Add Website of Docsauraus

---
+++ website/blog/javascript-basics.md
@@ -0,0 +1,9 @@
+---
+title: JavaScript Basics
+author: Venkat.R
+authorURL: http://webslate.io
+---
+
+This is a test post.
+
+A whole bunch of other information.
---
+++ website/core/Footer.js
@@ -0,0 +1,115 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require('react');
+
+class Footer extends React.Component {
+  docUrl(doc, language) {
+    const baseUrl = this.props.config.baseUrl;
+    const docsUrl = this.props.config.docsUrl;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+    const langPart = `${language ? `${language}/` : ''}`;
+    return `${baseUrl}${docsPart}${langPart}${doc}`;
+  }
+
+  pageUrl(doc, language) {
+    const baseUrl = this.props.config.baseUrl;
+    return baseUrl + (language ? `${language}/` : '') + doc;
+  }
+
+  render() {
+    return (
+      <footer className="nav-footer" id="footer">
+        <section className="sitemap">
+          <a href={this.props.config.baseUrl} className="nav-home">
+            {this.props.config.footerIcon && (
+              <img
+                src={this.props.config.baseUrl + this.props.config.footerIcon}
+                alt={this.props.config.title}
+                width="66"
+                height="58"
+              />
+            )}
+          </a>
+          <div>
+            <h5>Basics</h5>
+            <a href={this.docUrl('doc1.html', this.props.language)}>
+              HTML5
+            </a>
+            <a href={this.docUrl('doc2.html', this.props.language)}>
+              CSS3
+            </a>
+            <a href={this.docUrl('doc3.html', this.props.language)}>
+              JavaScript
+            </a>
+          </div>
+          <div>
+            <h5>Tools / Frameworks</h5>
+            <a href={this.pageUrl('users.html', this.props.language)}>
+              NodeJS
+            </a>
+            <a
+              href="https://stackoverflow.com/questions/tagged/"
+              target="_blank"
+              rel="noreferrer noopener">
+              ReactJS
+            </a>
+            <a href="https://discordapp.com/">Project Chat</a>
+            <a
+              href="https://twitter.com/"
+              target="_blank"
+              rel="noreferrer noopener">
+              Material UI
+            </a>
+          </div>
+          <div>
+            <h5>About Authors</h5>
+            <a href="https://www.linkedin.com/in/venkatarchitect/">Venkat.R</a>
+            <a href="https://github.com/">Karthikeyan</a>
+            <a href="https://github.com/">Shankar</a>
+            <a
+              className="github-button"
+              href={this.props.config.repoUrl}
+              data-icon="octicon-star"
+              data-count-href="/facebook/docusaurus/stargazers"
+              data-show-count="true"
+              data-count-aria-label="# stargazers on GitHub"
+              aria-label="Star this project on GitHub">
+              Star
+            </a>
+            {this.props.config.twitterUsername && (
+              <div className="social">
+                <a
+                  href={`https://twitter.com/${this.props.config.twitterUsername}`}
+                  className="twitter-follow-button">
+                  Follow @{this.props.config.twitterUsername}
+                </a>
+              </div>
+            )}
+            {this.props.config.facebookAppId && (
+              <div className="social">
+                <div
+                  className="fb-like"
+                  data-href={this.props.config.url}
+                  data-colorscheme="dark"
+                  data-layout="standard"
+                  data-share="true"
+                  data-width="225"
+                  data-show-faces="false"
+                />
+              </div>
+            )}
+          </div>
+        </section>
+
+        <section className="copyright">{this.props.config.copyright}</section>
+      </footer>
+    );
+  }
+}
+
+module.exports = Footer;
